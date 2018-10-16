1024e6426ded0f9ae38189944122c7978348fc16
Finacio <jeferson.inacio.2@gmail.com>
2018-10-16 19:01:37 -0300

    up again

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PageHeader from "../templates/pageHeader";
+
+export default props => (
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa Historia</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia in illum,
+      minus, quaerat quis saepe nobis eius rerum doloribus perspiciatis
+      necessitatibus eos provident? Provident quaerat reiciendis laborum
+      corporis voluptates aut.
+    </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente natus
+      sunt nihil doloribus, praesentium excepturi voluptates vel architecto
+      porro consectetur assumenda consequatur eum reiciendis ab quis asperiores
+      quas, amet maxime?
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non tenetur nisi
+      repellat placeat excepturi nesciunt voluptatem sed nam pariatur labore qui
+      accusantium itaque earum magni, veniam nihil voluptatibus magnam sint?
+    </p>
+  </div>
+);
